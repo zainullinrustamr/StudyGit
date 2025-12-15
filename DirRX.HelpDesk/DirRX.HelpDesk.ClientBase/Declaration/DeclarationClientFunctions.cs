fa7908fe912c58030c0e378e3320942e1c38947f
DirectumRXUser <directumrxuser@c.ru>
2025-12-15 21:57:03 +0400

    Создан документ "Декларация" [Rx]

---
+++ DirRX.HelpDesk/DirRX.HelpDesk.ClientBase/Declaration/DeclarationClientFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using DirRX.HelpDesk.Declaration;
+
+namespace DirRX.HelpDesk.Client
+{
+  partial class DeclarationFunctions
+  {
+
+  }
+}
