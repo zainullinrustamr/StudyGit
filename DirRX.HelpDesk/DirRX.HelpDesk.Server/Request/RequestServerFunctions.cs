6ff12724eb9badce014fba00af4e25defa0f1077
zainullinrustamr <zainullinrustamr@gmail.com>
2025-12-16 12:45:28 +0400

    Лабораторная работа 4 (#1)
    * Настройка репозитория [Rx]
    
    * Task 1.1 [Rx]
    
    * Добавил справочник "Внешнее обращение" [Rx]
    
    * Создан документ "Декларация" [Rx]
    
    * Создан документ "Приложение к обращению" [Rx]
    
    * Доработан документ AddendumRequest
    
    * Доработан справочник TypeRequest
    
    ---------
    
    Co-authored-by: DirectumRXUser <directumrxuser@c.ru>

---
+++ DirRX.HelpDesk/DirRX.HelpDesk.Server/Request/RequestServerFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using DirRX.HelpDesk.Request;
+
+namespace DirRX.HelpDesk.Server
+{
+  partial class RequestFunctions
+  {
+
+  }
+}
---
+++ DirRX.HelpDesk/DirRX.HelpDesk.Shared/AddendumRequest/AddendumRequestConstants.cs
@@ -0,0 +1,10 @@
+using System;
+using Sungero.Core;
+
+namespace DirRX.HelpDesk.Constants
+{
+  public static class AddendumRequest
+  {
+
+  }
+}
